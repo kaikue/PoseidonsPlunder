ae4176f6eb9a693fb749ff41ecad01edce3b9b39
kaikue <kvk@pobox.com>
2018-10-25 13:33:59 -0400

    switch to gamemode

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include "Load.hpp"
 
 //The 'GameMode' mode plays the game:
-//#include "GameMode.hpp"
+#include "GameMode.hpp"
 
-#include "MainMode.hpp"
+//#include "MainMode.hpp"
 
 #include "Connection.hpp"
 
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	//Client client(argv[1], argv[2]);
+	Client client(argv[1], argv[2]);
 
 	//------------  initialization ------------
 
@@ -120,8 +120,8 @@ int main(int argc, char **argv) {
 
 	//------------ create game mode + make current --------------
 
-	//Mode::set_current(std::make_shared< GameMode >(client));
-	Mode::set_current(std::make_shared< MainMode >());
+	Mode::set_current(std::make_shared< GameMode >(client));
+	//Mode::set_current(std::make_shared< MainMode >());
 
 	//------------ main loop ------------
 
